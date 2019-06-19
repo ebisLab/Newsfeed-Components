d22c4b954da5223af0c0d0aab1f335754edaa4cc
ebisLab <ebaiden@techie.com>
2019-06-19 17:44:25 -0400

    toggle open / close

--- Article/Article.js
+++ Article/Article.js
@@ -5,15 +5,26 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent ='expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this))
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+
+console.log('something')
+if (this.expandButton.textContent === 'expand'){
+  this.expandButton.textContent = 'close';
+  this.domElement.classList.toggle('article-open')
+}
+else if (this.expandButton.textContent === 'close') {
+  this.expandButton.textContent = 'expand';
+  this.domElement.classList.toggle('article-open')
+}
+
 
   }
 }
