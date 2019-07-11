607d812b90bc355b97946af859373f9f6b55d4be
ebisLab <ebaiden@techie.com>
2019-07-10 20:34:45 -0400

    toggle classes

--- Article/Article.js
+++ Article/Article.js
@@ -17,25 +17,55 @@ const articleData = [
 
 const grabArticleSection = document.querySelector('.articles');
 articleData.forEach(data=> {
-  grabArticleSection.appendChild(createArticleComponent(data.title, data.blog))
+  grabArticleSection.appendChild(createArticleComponent(data.title, data.date, data.blog))
 })
 
-grabArticleSection.appendChild(createArticleComponent('hello world', 'My there'))
-function createArticleComponent(){
+//grabArticleSection.appendChild(createArticleComponent('hello world', 'My there'))
+function createArticleComponent(title, date, blog){
   const articleSection = document.createElement('div');
   const articleTitle= document.createElement('h2');
   const dateParagraph= document.createElement('p');
   const articleParagraph= document.createElement('p');
-  const expandButton= document.createElement('span')
+  const expandButton= document.createElement('span');
+  const closeButton= document.createElement('span');
   
   articleSection.appendChild(articleTitle)
   articleSection.appendChild(dateParagraph);
   articleSection.appendChild(articleParagraph)
   articleSection.appendChild(expandButton)
+  expandButton.appendChild(closeButton)
 
   articleSection.classList.add('article');
   dateParagraph.classList.add('date')
   expandButton.classList.add('expandButton')
+  closeButton.classList.add('closeButton')
+
+  expandButton.innerText='expand';
+  closeButton.innerText='close';
+
+  articleTitle.innerText= title;
+  dateParagraph.innerText = date;
+  articleParagraph.innerText = blog;
+
+  expandButton.addEventListener('click', event =>{
+console.log('Button clicked')
+//expandButton.classList.toggle('close')
+//closeButton.classList.toggle('close')
+if (event.target.innerText === 'expand'){
+  event.target.innerText = 'close';
+  articleSection.classList.toggle('article-open')
+}
+else if (event.target.innerText === 'close'){
+  event.target.innerText = 'expand';
+  articleSection.classList.toggle('article-open')
+}
+
+//articleSection.classList.toggle('article-open')
+
+
+  })
+
+
 
   return articleSection
 
